fa3f0c28f04ed1cbd32202b8f1c591bf4a43fae9
Andres Felipe Velandia Erazo <avelande@emeal.nttdata.com>
2024-10-17 17:26:17 -0500

    The algorithm is updated to compare the value of the selected property with the properties file so that the value of the property id is assigned to the new field

--- src/csvProcessor.js
+++ src/csvProcessor.js
@@ -5,33 +5,56 @@ import { processMigration } from './processMigrationHelpers.js';
 import path from 'path';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
+import { log } from 'console';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const inputFilePath = path.join(__dirname, 'test.csv');
+const inputpropertiesFilePath = path.join(__dirname, 'properties.csv');
 const outputFilePath = path.join(__dirname, 'test_output.csv');
 
 const records = [];
+const properties = [];
 
+ const readProperties = async () => {
+  return new Promise((resolve, reject) => {
+    const readProperties = fs.createReadStream(inputpropertiesFilePath);
+    readProperties
+    .pipe(csvParser({ separator: ';' }))
+    .on('data', (row) => {
+      const finalProp = {id: row['PropertyTypes ID'], name:row.PropertyTypes }
+      properties.push(finalProp)
+    })
+      .on('end', () => {
+        resolve(properties);
+      })
+      .on('error', (error) => {
+        reject(error);
+      });
+  });
+
+ }
 const readCSV = async () => {
+ 
+  const finalProperties = await readProperties()
+  
   return new Promise((resolve, reject) => {
     const readStream = fs.createReadStream(inputFilePath);
     readStream
       .pipe(csvParser({ separator: ';' }))
       .on('data', (row) => {
-        const rawFilterColumn = row['raw_filter'];
-        row['raw_filter_updated']='Error'
-        try {
-            const parsedData = rawFilterColumn.charAt(rawFilterColumn.length - 1) === ']'? JSON.parse(rawFilterColumn) : 'error'
-            const updatedObj = processMigration(parsedData);
-            row['raw_filter_updated'] = JSON.stringify(updatedObj);
-            records.push(row);
-        } catch (error) {
-          console.log('error', row);
-          records.push({...row});
-        }      
-        
+          const rawFilterColumn = row['raw_filter'];
+          row['is_snmp']='Error'
+          try {
+              const parsedData = rawFilterColumn.charAt(rawFilterColumn.length - 1) === ']'? JSON.parse(rawFilterColumn) : 'error'
+              const updatedObj = processMigration(parsedData, finalProperties);
+              row['is_snmp'] = JSON.stringify(updatedObj);
+              records.push(row);
+          } catch (error) {
+            console.log('error', row);
+            records.push({...row});
+          }      
       })
       .on('end', () => {
         resolve(records);
--- src/processMigrationHelpers.js
+++ src/processMigrationHelpers.js
@@ -18,23 +18,25 @@ const changePropertiesDeviceNameToParentName = (item) => {
     }
 }
 
-const addNewFields = (item) => {
+const addNewFields = (item,finalProperties) => {
     if (!item.hasOwnProperty('height')) {
         item.height = 15; // Valor por defecto
       }
  
       if (!item.hasOwnProperty('selectedPropertyId')) {
-        item.selectedPropertyId = ""; // Valor por defecto
+        const value = finalProperties.find(fp => fp.name === item.selectedProperty)?.id
+        
+        item.selectedPropertyId = value || ""; // Valor por defecto
       }
  
       if (!item.hasOwnProperty('InputForEquipmentId')) {
-        item.InputForEquipmentId = ""; // Valor por defecto
+        item.InputForEquipmentId = "124554051601"; // Valor por defecto
       }
       return item;
 }
 
 
-export const processMigration = (data) => {
+export const processMigration = (data, finalProperties) => {
     try {
         let stringifiedData = JSON.stringify(data);
 
@@ -51,7 +53,7 @@ export const processMigration = (data) => {
                 const rawFilterWithOutPropertiesOption = deletePropertiesOption(item);                
                 if(item.name === 'equipmentProperty') {
                     const rawFilterWithParentRule = changePropertiesDeviceNameToParentName(rawFilterWithOutPropertiesOption);
-                    return addNewFields(rawFilterWithParentRule);
+                    return addNewFields(rawFilterWithParentRule, finalProperties);
                 }
                 for (let key in item) {
                     if (item.hasOwnProperty(key)) {
@@ -76,3 +78,44 @@ export const processMigration = (data) => {
 
 }
 
+
+
+const validationSNMP = (item) => {
+    if (!item.inputForEquipment || item.inputForEquipment === 'SNMP-Interface') {
+         item = ['IS_SNMP']
+    } else {
+        item = ['is_not_snmp']
+    }
+    return item
+}
+
+export const processValidationSNMP = (data) => {
+    try {
+        let stringifiedData = JSON.stringify(data);
+        if (stringifiedData.charAt(stringifiedData.length - 1) !== ']') {
+            return ["ERROR"]
+        }
+        let parsedData = JSON.parse(stringifiedData);
+        let counterSnmp = 0;
+        const processItem = (item) => {
+            if (typeof item === 'object') {
+                const arrayValidationSNMP = validationSNMP(item);
+                if(arrayValidationSNMP[0] ==='is_not_snmp') counterSnmp++;
+                for (let key in item) {
+                    if ( item[key].hasOwnProperty('inputForEquipment') && item.hasOwnProperty(key)) {
+                        item[key] = processItem(item[key]);
+                    }
+                  }
+                  return counterSnmp < 1?[ 'IS_SNMP' ] : [ 'is_not_snmp' ];
+            } else {
+                return item
+            }
+        }
+        return processItem(parsedData);
+
+    } catch (error) {
+        console.error("Error al parsear los datos:", error);
+        return null;
+    }
+
+}
